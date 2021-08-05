198c2a4d2415eb41f5fe9ba0e35438c5bb3303b7
Bulkovsky Vaslisy <farmazon4er@gmail.com>
2021-08-05 12:44:07 +0500

    add users and devise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   include ImageUploader::Attachment.new(:image)
+  belongs_to :user
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, length: { maximum: 150 }
   validates :image_data, presence: true
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :posts
+  CONST_VALID_EMAIL = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 100},
+            format: { with: CONST_VALID_EMAIL }
+
+end
