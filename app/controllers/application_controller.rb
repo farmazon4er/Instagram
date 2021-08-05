64d830d96fefc2653bc75f06f040750065b2636c
Bulkovsky Vaslisy <farmazon4er@gmail.com>
2021-08-05 16:33:52 +0500

    add user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(user)
+    user_posts_path(user)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sing_up, keys: [:name, :bio])
+  end
+
+  private
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
 end
