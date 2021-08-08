1cb7a2c3fbb398fda11e97ee5760410e5aaf0d71
Bulkovsky Vaslisy <farmazon4er@gmail.com>
2021-08-08 18:03:59 +0500

    upload valid

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,13 @@
 class ImageUploader < Shrine
   plugin :default_url
-
-
+  plugin :validation_helpers
 
   Attacher.default_url do |**options|
     "/placeholders/missing.jpg"
+  end
+
+  Attacher.validate do
+    validate_extension %w[jpg jpeg png webp]
   end
 
 end
