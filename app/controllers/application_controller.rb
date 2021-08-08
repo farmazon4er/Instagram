6520a13c5fe2f87b1ed31f2346b6b87f8b29c5fb
Bulkovsky Vaslisy <farmazon4er@gmail.com>
2021-08-08 19:20:06 +0500

    edit fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
     user_posts_path(user)
   end
 
-
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio])
   end
