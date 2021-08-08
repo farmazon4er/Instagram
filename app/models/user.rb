8800147dd3faec654fee2d0bde17bff2b4811835
Bulkovsky Vaslisy <farmazon4er@gmail.com>
2021-08-08 19:42:29 +0500

    depend_destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :follower_follows, foreign_key: :following_id, class_name: "Follow"
-  has_many :followers, through: :follower_follows, source: :follower
+  has_many :follower_follows, foreign_key: :following_id, class_name: "Follow", dependent: :destroy
+  has_many :followers, through: :follower_follows, source: :follower, dependent: :destroy
 
-  has_many :following_follows, foreign_key: :follower_id, class_name: "Follow"
-  has_many :followings, through: :following_follows, source: :following
+  has_many :following_follows, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
+  has_many :followings, through: :following_follows, source: :following, dependent: :destroy
 
   CONST_VALID_EMAIL = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
