6c3d48173c235e1bdd253a7d30c694e45da934a8
Bulkovsky Vaslisy <farmazon4er@gmail.com>
2021-08-10 21:48:09 +0500

    added post_controller_spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,7 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'capybara/rspec'
 require 'shoulda/matchers'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
@@ -94,4 +95,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+
+
 end
