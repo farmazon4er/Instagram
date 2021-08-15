829dd574f736051471fa9a64219d9fb1291da101
Bulkovsky Vaslisy <farmazon4er@gmail.com>
2021-08-15 18:34:04 +0500

    comment test create and destroy

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -37,6 +37,43 @@ RSpec.describe CommentsController, type: :controller do
     end
   end
 
+  describe '#create' do
+    let(:params) do
+      {
+        user_id: user.id,
+        post_id: post.id,
+        comment: attributes_for(:comment)
+      }
+    end
 
+    subject { get :create, params: params }
+
+    it 'create comment' do
+      expect { subject }.to change { Comment.count }.by(1)
+      is_expected.to redirect_to(user_post_path(assigns(:user), assigns(:post)))
+    end
+
+    context 'when params invalid' do
+      let(:params) do
+        { user_id: user.id, post_id: post.id, comment: { comment_text: nil } }
+      end
+
+      it { is_expected.to render_template :new }
+      it { expect { subject}.not_to change { Comment.count } }
+    end
+
+  end
+
+  describe '#destroy' do
+    let!(:comment) { create(:comment, user: user, post: post) }
+    let(:params) { { user_id: user.id, post_id: post.id, id: comment.id } }
+    subject { process :destroy, method: :delete, params: params }
+
+    it 'destroy record' do
+      expect { subject }.to change { Comment.count }.by(-1)
+    end
+
+    it { is_expected.to redirect_to(user_post_comments_path(assigns(:user), assigns(:post))) }
+  end
 
 end
